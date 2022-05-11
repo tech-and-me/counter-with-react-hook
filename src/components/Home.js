4f8a45f86ea7bc21a52adedeee7576673119f52b
Pary <81phary@gmail.com>
2022-05-11 12:54:51 +1000

    adding useEffect and axios fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return <h1 className="text-danger">This is HOME Page</h1>;
+  const [userList, setUserList] = useState([]);
+  const fetchMoviesList = async () => {
+    const result = await axios.get(
+      `https://jsonplaceholder.typicode.com/users`
+    );
+    console.log(result);
+    setUserList(result.data);
+  };
+
+  useEffect(() => {
+    fetchMoviesList();
+  }, [userList]);
+
+  return (
+    <div>
+      <h1 className="text-danger">This is HOME Page</h1>
+      <h1>Fetch results</h1>
+      <hr />
+      <div>
+        {userList.map((user) => {
+          return (
+            <div key={user.id}>
+              {user.name} | {user.email}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
